4d424b18995e8b6a14fcfabe1cb376ac29bfd65a
megha <megha.patel2@sheridancollege.ca>
2019-05-13 13:02:27 -0400

    new project

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author Megha
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] a=new int[5];
+        
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        a[3]=40;
+        a[4]=50;
+        
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+    
+}
